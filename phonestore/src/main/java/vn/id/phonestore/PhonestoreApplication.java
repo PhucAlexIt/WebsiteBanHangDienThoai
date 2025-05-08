e49cc3e5936a099e361a1f1a36880b9d28ce8b40
PhamThanhTai-22130242 <22130242@st.hcmuaf.edu.vn>
2025-05-08 18:30:58 +0700

    commit dau tien

---
+++ phonestore/src/main/java/vn/id/phonestore/PhonestoreApplication.java
@@ -0,0 +1,13 @@
+package vn.id.phonestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonestoreApplication.class, args);
+	}
+
+}
