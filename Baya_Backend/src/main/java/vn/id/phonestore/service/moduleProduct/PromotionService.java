315967b6827c8516f090dd6efc269045c91993da
ThuyThanhN <22130283@st.hcmuaf.edu.vn>
2025-05-14 22:35:27 +0700

    viet entity Promotion và repository cho Promotion lay tat ca promotion va them promotion

---
+++ Baya_Backend/src/main/java/vn/id/phonestore/service/moduleProduct/PromotionService.java
@@ -0,0 +1,35 @@
+package vn.id.phonestore.service.moduleProduct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import vn.id.phonestore.dtos.PromotionDTO;
+import vn.id.phonestore.entity.Promotion;
+import vn.id.phonestore.repository.moduleProduct.PromotionRepository;
+
+import java.time.Instant;
+import java.util.List;
+
+@Service
+public class PromotionService {
+    @Autowired
+    private PromotionRepository promotionRepository;
+
+    public List<Promotion> getAllPromotion() {
+        return promotionRepository.findAll();
+    }
+
+    public Promotion createPromotion(PromotionDTO dto) {
+        Promotion promotion = new Promotion();
+        promotion.setName(dto.getName());
+        promotion.setDescription(dto.getDescription());
+        promotion.setDiscountValue(dto.getDiscountValue());
+        promotion.setStartDate(dto.getStartDate());
+        promotion.setEndDate(dto.getEndDate());
+        promotion.setStatus(dto.getStatus() != null ? dto.getStatus() : 1);
+        promotion.setCreatedAt(Instant.now());
+        promotion.setUpdatedAt(Instant.now());
+
+        return promotionRepository.save(promotion);
+    }
+
+}
