f1e09d6280e48e3137cc486e836c1a0687017366
PhucAlexIt <phucalex347@gmail.com>
2025-05-17 21:48:12 +0700

    fix lai

--- Baya_Backend/src/main/java/vn/id/phonestore/repository/moduleUser/UserRepository.java
+++ Baya_Backend/src/main/java/vn/id/phonestore/repository/moduleUser/UserRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    User getUserByUserID(Integer userID);
+    User getUserById(Integer id);
     List<User> findByFullName(String fullName);
 
 }
